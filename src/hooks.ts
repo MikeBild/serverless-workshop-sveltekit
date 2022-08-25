affc999cff75afd4059eac64e2e85428398ffe96
Mike Bild <mike.bild.ext@siemens.com>
2022-08-25 07:01:52 +0200

    add json web token validation on signin

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,14 +1,32 @@
 import type { ExternalFetch, Handle, HandleError } from '@sveltejs/kit';
 import { parse } from 'cookie';
 import { decode, type JwtPayload } from 'jsonwebtoken';
+import { CognitoJwtVerifier } from 'aws-jwt-verify';
+import { USERPOOLID } from '$env/static/private';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const cookie = parse(event.request.headers.get('cookie') || '');
-	const payload = decode(cookie.token) as JwtPayload;
+	const token = cookie.token;
+	const payload = decode(token) as JwtPayload;
+	const groups = payload ? payload['cognito:groups'] : undefined;
+	const clientId = payload?.client_id as string;
+	const username = payload?.username;
 
-	event.locals.accessToken = cookie.token;
-	event.locals.username = payload?.username;
-	event.locals.usergroups = payload ? payload['cognito:groups'] : undefined;
+	try {
+		const verifier = CognitoJwtVerifier.create({
+			userPoolId: USERPOOLID,
+			tokenUse: 'access'
+		});
+		const payload = await verifier.verify(token, { clientId, groups });
+
+		event.locals.accessToken = token;
+		event.locals.username = username;
+		event.locals.usergroups = groups;
+	} catch {
+		event.locals.accessToken = undefined;
+		event.locals.username = undefined;
+		event.locals.usergroups = undefined;
+	}
 
 	return await resolve(event);
 };
