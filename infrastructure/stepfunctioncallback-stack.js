ca03fd7780379127a993a652866ca41e61ac1d57
Mike Bild <mike@mikebild.com>
2022-08-25 17:02:20 +0200

    fix sf exection

--- infrastructure/stepfunctioncallback-stack.js
+++ infrastructure/stepfunctioncallback-stack.js
@@ -75,8 +75,7 @@ exports.handler = async (event, context) => {
 			}).next(new aws_stepfunctions.Succeed(this, 'done'))
 		});
 
-		this.stateMachine.grantTaskResponse(props.serverHandler);
-		this.stateMachine.grantStartExecution(props.serverHandler);
+		this.stateMachine.grant(props.serverHandler, ['states:*']);
 
 		new CfnOutput(this, 'StateMachineArn', {
 			value: this.stateMachine.stateMachineArn
